8fbae9d2f41eabbc35caf7208b576a5cc2a0fc26
Hayness <zerossb@gmail.com>
2026-05-27 14:21:05 -0300

    Update PatchController.php

--- src/Http/Controllers/Users/Notifications/PatchController.php
+++ src/Http/Controllers/Users/Notifications/PatchController.php
@@ -14,16 +14,15 @@ class PatchController extends NotificationsController
         $params = [
             'project' => Config::get('app.slug', ''),
         ];
-        $token_uri = Config::get('hub.base_uri').Config::get('hub.prefix').Config::get('hub.oauth.notifications_uri').'?'.http_build_query($params);
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-            'Authorization' => $request->headers->get('Authorization'),
-        ])->patch(
-            $token_uri,
-            [
-                'mark_all_as_read' => $request->get('mark_all_as_read'),
-            ]
-        );
+        $token_uri = Config::get('hub.base_uri') . Config::get('hub.prefix') . Config::get('hub.oauth.notifications_uri') . '?' . http_build_query($params);
+        $response = Http::acceptJson()
+            ->withToken($bearerToken)
+            ->patch(
+                $token_uri,
+                [
+                    'mark_all_as_read' => $request->get('mark_all_as_read'),
+                ]
+            );
 
         return new Response($response, $response->status());
     }
