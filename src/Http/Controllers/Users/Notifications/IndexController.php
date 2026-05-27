835c3bc197b8894ffc467678b3ee42fb15d64232
Hayness <zerossb@gmail.com>
2026-05-27 14:20:54 -0300

    Update IndexController.php

--- src/Http/Controllers/Users/Notifications/IndexController.php
+++ src/Http/Controllers/Users/Notifications/IndexController.php
@@ -14,13 +14,12 @@ class IndexController extends NotificationsController
         $params = [
             'project' => Config::get('app.slug', ''),
         ];
-        $token_uri = Config::get('hub.base_uri').Config::get('hub.prefix').Config::get('hub.oauth.notifications_uri').'?'.http_build_query($params);
-        $response = Http::withHeaders([
-            'Accept' => 'application/json',
-            'Authorization' => $request->headers->get('Authorization'),
-        ])->get(
-            $token_uri
-        );
+        $token_uri = Config::get('hub.base_uri') . Config::get('hub.prefix') . Config::get('hub.oauth.notifications_uri') . '?' . http_build_query($params);
+        $response = Http::acceptJson()
+            ->withToken($bearerToken)
+            ->get(
+                $token_uri
+            );
 
         return new Response($response, $response->status());
     }
